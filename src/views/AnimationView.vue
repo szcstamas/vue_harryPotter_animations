07d5641145091770ff2e6d4233e4a785cbd914a7
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-06-13 14:53:09 +0200

    Adding Velocity dependency into project and using it with custom JS hooks in AnimationView component

--- src/views/AnimationView.vue
+++ src/views/AnimationView.vue
@@ -1,6 +1,9 @@
 <template>
   <section>
     <div class="vue-animate">
+      <h4 class="vue-animation-h4">
+        Basic animation using transition-component
+      </h4>
       <button @click="toggleModal">Open</button>
       <transition name="fade">
         <div v-if="isOpen" class="modal">
@@ -9,27 +12,61 @@
       </transition>
     </div>
 
-    <hr style="margin: 4rem 0;">
+    <hr style="margin: 4rem 0" />
 
     <div class="vue-animate input-container">
-          <input v-model="newContact" placeholder="Name" type="text" />
-          <button @click="addContact">Add Contact</button>
-          <button @click="sortContact">Sort Contact</button>
+      <h4 class="vue-animation-h4">Using transition-group component</h4>
+      <input v-model="newContact" placeholder="Name" type="text" />
+      <button @click="addContact">Add Contact</button>
+      <button @click="sortContact">Sort Contact</button>
 
-          <TransitionGroup class="vue-animated-input-container" name="slide-up" tag="ul" appear>
-            <li v-for="(contact, index) in contacts" :key="index">
-              {{ contact }}
-            </li>
-          </TransitionGroup>
+      <TransitionGroup
+        class="vue-animated-input-container"
+        name="slide-up"
+        tag="ul"
+        appear
+      >
+        <li v-for="(contact, index) in contacts" :key="index">
+          {{ contact }}
+        </li>
+      </TransitionGroup>
+    </div>
+
+    <hr style="margin: 4rem 0" />
+
+    <div class="input-container drawer-container">
+      <h4 class="vue-animation-h4">Advanced animations using JS Hooks with Velocity</h4>
+      <button @click="isDrawerOpen = !isDrawerOpen">My Profile</button>
+
+      <transition
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @leave="leave"
+        :css="false"
+      >
+        <div v-if="isDrawerOpen" class="drawer">
+          <img
+            src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+            alt="avatar"
+          />
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </transition>
     </div>
   </section>
 </template>
 
 <script>
+import Velocity from "velocity-animate";
+
 export default {
   data() {
     return {
       isOpen: false,
+      isDrawerOpen: false,
       newContact: "",
       contacts: ["Beau Thabeast", "Cindy Rella", "Alice Wunderlind"],
     };
@@ -44,8 +81,30 @@ export default {
       this.newContact = "";
     },
     sortContact() {
-      this.contacts.sort()
-    }
+      this.contacts.sort();
+    },
+    beforeEnter(el) {
+      el.style.opacity = 0;
+      el.style.width = "0em";
+    },
+    enter(el, done) {
+      Velocity(
+        // element to animate
+        el,
+        // animation properties
+        { opacity: 1, width: "12em" },
+        { duration: 1000, easing: [20, 5], complete: done }
+      );
+    },
+    leave(el, done) {
+      Velocity(
+        // element to animate
+        el,
+        // animation properties
+        { opacity: 0, width: "0em" },
+        { duration: 500, easing: "easeInCubic", complete: done }
+      );
+    },
   },
 };
 </script>
@@ -109,6 +168,50 @@ ul {
 
 ul li {
   font-size: 1.25rem;
-  margin: .25rem;
+  margin: 0.25rem;
+}
+</style>
+
+// LOCAL STYLES
+<style scoped>
+img {
+  height: 2.5em;
+  width: 2.5em;
+  border-radius: 50%;
+}
+
+.drawer-container {
+  text-align: center;
+}
+
+.drawer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 12em;
+  height: 20em;
+  border-radius: 1%;
+  background-color: #e0e0e0;
+  box-shadow: 0.08em 0.03em 0.4em #ababab;
+  padding-top: 0.7em;
+}
+
+.drawer div {
+  height: 3.5em;
+  width: 95%;
+  margin-top: 0.6em;
+  background-color: #f0f0f0;
+  border: 0.02em solid #ababab;
+  border-radius: 1%;
+}
+
+.vue-animation-h4 {
+  background-color: #efefef;
+  padding: 1rem;
+  border-radius: 10px;
+
+  width: 50%;
+  margin: auto;
+  margin-bottom: 2rem;
 }
 </style>
